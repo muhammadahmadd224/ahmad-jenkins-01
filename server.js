a5c64fcfb4d3dc869079ba73cb7be483d141442e
muhammadahmadd224 <ym833641@gmail.com>
2025-11-11 22:47:43 +0500

    hhh

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Dockerized Node.js App! done by ahmadkhan and now check through cicd pipeline  ');
+  res.send('Hello from Dockerized Node.js App! done by ahmadkhan and now check through cicd pipeline ..  ');
 });
 
 app.listen(port, () => {
